2f6067ae5240a95f9088b5a378e6cdffd6babbf2
Ahmad R. Satvati <ahmadsatvati10@gmail.com>
2024-02-07 22:02:29 +0330

    Options Overlay and Scriptable Objects
    Created a bunch of characters and balanced their abilities
    Created the logic for settings, the audio is not implemented yet, time to create the game itself

--- Assets/Scripts/OptionsOverlayController.cs
+++ Assets/Scripts/OptionsOverlayController.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsOverlayController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Character Selection UI")]
+    public Image CharacterImage;
+    public TextMeshProUGUI CharacterNameText;
+
+    private int selectedCharacterIndex = 0;
+
+    #region Unity Callbacks
+    private void Awake()
     {
-        
+        selectedCharacterIndex = PlayerPrefs.GetInt("Player_Character");
+        UpdateCharacterIndex(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Public Methods
+
+    public void UpdateCharacterIndex(int addedIndex)
     {
-        
+        selectedCharacterIndex += addedIndex;
+        if (selectedCharacterIndex < 0)
+            selectedCharacterIndex += RecordKeeper.Instance.AvailableCharacters.Count;
+        selectedCharacterIndex %= RecordKeeper.Instance.AvailableCharacters.Count;
+        CharacterImage.sprite = RecordKeeper.Instance.AvailableCharacters[selectedCharacterIndex].CharacterImage;
+        CharacterNameText.text = RecordKeeper.Instance.AvailableCharacters[selectedCharacterIndex].CharacterName;
+        PlayerPrefs.SetInt("Player_Character", selectedCharacterIndex);
     }
+    public void SetMusicValue(float value)
+    {
+
+    }
+    public void SetAudioValue(float value)
+    {
+
+    }
+    public void SetMusicState(bool state)
+    {
+
+    }
+    public void SetAudioState(bool state)
+    {
+
+    }
+    public void ShareTheGame()
+    {
+
+    }
+    public void RateUs()
+    {
+
+    }
+    #endregion
 }
--- Assets/Scripts/RecordKeeper.cs
+++ Assets/Scripts/RecordKeeper.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class RecordKeeper : MonoBehaviour
 {
+    public static RecordKeeper Instance { get; private set; }
+
+
     [Header("Global Game Settings")]
     public List<CharacterScriptableObject> AvailableCharacters = new List<CharacterScriptableObject>();
+
+
+    #region Unity Callbacks
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(Instance);
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+        if (!PlayerPrefs.HasKey("Player_Character"))
+            PlayerPrefs.SetInt("Player_Character", 0);
+    }
+    #endregion
 }
